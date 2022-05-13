4a6ffa4674119d2f77264f763429c72aaa8f724b
vthelamb <vglamb8@gmail.com>
2022-05-13 17:36:36 -0300

    Built Header component

---
+++ compareyourair/myapp/dept-app/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header(){
+  return (
+    <div>
+      <h1>Compare your Air</h1>
+      <br></br>
+      <h2>Compare the air quality between cities in the UK.</h2>
+      <h2>Select cities to compare using the search tool below.</h2>
+      <br></br>
+    </div>
+  )
+}
+  
+  export default Header;
